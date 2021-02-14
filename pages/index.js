9d12b54bb995240227a5603bba2d3175dca7f2b5
vonbarown <vonielbrown@Voniels-MacBook-Air.local>
2021-02-13 20:12:48 -0500

    add twitter feed

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import MapChart from './maps/MapChart';
 
-
 export default function Home() {
   return (
     <div>
--- pages/twitterFeed/[screenName].js
+++ pages/twitterFeed/[screenName].js
@@ -1,34 +1,34 @@
-import { Timeline } from 'react-twitter-widgets'
+import React from 'react'
+import { Timeline } from 'react-twitter-widgets';
 export async function getServerSideProps(context) {
-    console.log(context)
-    // const url = `https://dog.ceo/api/breeds/image/random`;
-    // const response = await fetch(url)
-    // const data = await response.json()
-    return {
-      props: {
-        screenName: context.params.screenName
-      }
-    }
-  }
-  
-  const TwitterFeed = ({  screenName }) => {
-    console.log(screenName)
-  
-    if (!screenName) return <h1>Loading...</h1>
-  
-    return (
-      <div>
+
+
+  return {
+    props: {
+      screenName: context.params.screenName,
+    },
+  };
+}
+
+const TwitterFeed = ({ screenName }) => {
+  console.log(screenName);
+
+  if (!screenName) return <h1>Loading...</h1>;
+
+  return (
+    <div>
       <Timeline
-  dataSource={{
-    sourceType: 'profile',
-    screenName: screenName
-  }}
-  options={{
-    height: '900'
-  }}
-/>
-      </div>
-    )
-  }
-  
-  export default TwitterFeed;
+        dataSource={{
+          sourceType: 'profile',
+          screenName: screenName,
+        }}
+        options={{
+          height: '900',
+          width:'700'
+        }}
+      />
+    </div>
+  );
+};
+
+export default TwitterFeed;
