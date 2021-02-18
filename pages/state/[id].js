0316c4f755a3166b22134850e2aaae5bb6e432e1
jenesh <jenesh@codecademy.com>
2021-02-17 21:17:39 -0500

    Turning into a grid

--- pages/state/[id].js
+++ pages/state/[id].js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import * as member from '../api/propublica/member';
 import CongressPerson from '../../components/CongressPerson';
 import NavBar from '../../components/NavBar/NavBar';
+import { States } from '../../assests/localized/state';
 
 export async function getServerSideProps(context) {
   const { id } = context.params;
@@ -34,25 +35,29 @@ const State = ({ id }) => {
 
   return (
     <div>
-      <div className="flex  justify-center">
+      <div className="flex justify-center pb-6">
         <NavBar />
       </div>
-      <h1>{id}</h1>
-      <div>
+      <h1 className="text-4xl pt-4 flex justify-center">{States[id]}</h1>
+      <div className="grid grid-cols-2 pt-4">
         <div>
-          <h1>Senators</h1>
+          <h1 className="text-xl pb-2 flex justify-center font-semibold tracking-wide">
+            Senators
+          </h1>
           {senators
             ? senators.map((el) => {
-                return <CongressPerson key={el.id} data={el} />;
-              })
+              return <CongressPerson key={el.id} data={el} />;
+            })
             : null}
         </div>
         <div>
-          <h1>Representatives</h1>
+          <h1 className="text-xl pb-2 pl-4 flex justify-center font-semibold tracking-wide">
+            Representatives
+          </h1>
           {representative
             ? representative.map((el) => {
-                return <CongressPerson key={el.id} data={el} />;
-              })
+              return <CongressPerson key={el.id} data={el} />;
+            })
             : null}
         </div>
       </div>
