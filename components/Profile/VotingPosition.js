0e65602747d34599fcad9c71fadb7a5d567ec463
vonbarown <bvoniel@gmail.com>
2021-02-17 21:48:51 -0500

    update votingPositionsyntax

--- components/Profile/VotingPosition.js
+++ components/Profile/VotingPosition.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const VotingPosition = ({ data }) => {
+  console.log('fd', Object.values(data.amendment).length);
   const className =
     'amendment-info rounded-lg border-gray-300 border-2 w-96 overFlow-auto overflow-x-scroll h-52';
-  if (data.amendment.length > 0 && data.bill.bill_id) {
+  if (Object.values(data.amendment).length !== 0 && data.bill.bill_id) {
     return (
       <div className={className}>
         <p>{data.date}</p>
@@ -14,22 +15,22 @@ const VotingPosition = ({ data }) => {
         <p> Vote Result: {data.result}</p>
       </div>
     );
-  } else {
-    return (
-      <div>
-        {data.bill.bill_id && (
-          <div className={className}>
-            <p>{data.date}</p>
-            <p>Bill Id: {data.bill.bill_id}</p>
-            <h2>Bill Description: {data.description}</h2>
-            <h3>Bill Title: {data.bill.title}</h3>
-            <p>Voting Position: {data.position}</p>
-            <p> Vote Result: {data.result}</p>
-          </div>
-        )}
-      </div>
-    );
   }
+
+  return (
+    <div>
+      {Object.values(data.bill).length !== 0 && data.bill.bill_id && (
+        <div className={className}>
+          <p>{data.date}</p>
+          <p>Bill Id: {data.bill.bill_id}</p>
+          <h2>Bill Description: {data.description}</h2>
+          <h3>Bill Title: {data.bill.title}</h3>
+          <p>Voting Position: {data.position}</p>
+          <p> Vote Result: {data.result}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default VotingPosition;
